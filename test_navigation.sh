3e9c70bac95d42c3dff76bb1242e6364dc0e80a5
Kuan-HC <blackshield33@gmail.com>
2020-08-27 11:56:50 +0000

    commit pick objects init

--- test_navigation.sh
+++ test_navigation.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-xterm  -e  "source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/HSR/src/world/my_world.world" &
+
+xterm  -e  "source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch " &
 sleep 5
 xterm  -e  "source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch" & 
 sleep 5
-xterm  -e  "source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+xterm  -e  "source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch"
 
