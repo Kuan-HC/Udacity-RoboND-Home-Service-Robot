b7c9d2bc8da98feb28dbc6f9f38d53c01d4ca981
Kuan-HC <blackshield33@gmail.com>
2020-08-28 19:34:56 +0800

    sh file

--- home_service.sh
+++ home_service.sh
@@ -5,4 +5,6 @@ xterm  -e  "source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch
 sleep 5
 xterm  -e  "source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 10
+xterm  -e  "source devel/setup.bash; rosrun pick_objects pick_objects" &
+sleep 3
 xterm  -e  "source devel/setup.bash; rosrun add_markers add_markers_hsr"
