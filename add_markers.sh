f23117aa7ec46fe902788a72646f4fe044d3b8c7
Kuan-HC <blackshield33@gmail.com>
2020-08-28 14:02:13 +0800

    add markers init

--- add_markers.sh
+++ add_markers.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-
 xterm  -e  "source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/HSR/src/world/my_world.world" &
-sleep 5 
+sleep 10
 xterm  -e  "source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/HSR/src/world/map.yaml" & 
 sleep 5
-xterm  -e  "source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch"
-
+xterm  -e  "source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 10
+xterm  -e  "source devel/setup.bash; osrun add_markers add_markers"
