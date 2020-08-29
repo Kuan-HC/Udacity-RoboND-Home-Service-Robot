29cb3d2fadae31682e045df535224f459c578a03
Kuan-HC <63347328+Kuan-HC@users.noreply.github.com>
2020-08-29 11:50:52 +0800

    Update home_service.sh

--- shell/home_service.sh
+++ shell/home_service.sh
@@ -3,7 +3,7 @@ xterm  -e  "source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.
 sleep 10
 xterm  -e  "source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/HSR/src/world/map.yaml" & 
 sleep 5
-xterm  -e  "source devel/setup.bash; rosrun rviz rviz -d /home/workspace/HSR/home_service.rviz" &
+xterm  -e  "source devel/setup.bash; rosrun rviz rviz -d /home/workspace/Udacity-RoboND-Home-Service-Robot/home_service.rviz" &
 sleep 10
 xterm  -e  "source devel/setup.bash; rosrun add_markers add_markers_hsr" &
 sleep 1
