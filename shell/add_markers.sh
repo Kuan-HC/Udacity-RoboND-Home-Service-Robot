af037e1c2cc9ea49c0503d58cedb184c20456e99
Kuan-HC <63347328+Kuan-HC@users.noreply.github.com>
2020-08-29 12:28:19 +0800

    Update add_markers.sh

--- shell/add_markers.sh
+++ shell/add_markers.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-xterm  -e  "source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/HSR/src/world/my_world.world" &
+xterm  -e  "source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/Udacity-RoboND-Home-Service-Robot/src/world/my_world.world" &
 sleep 10
-xterm  -e  "source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/HSR/src/world/map.yaml" & 
+xterm  -e  "source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/Udacity-RoboND-Home-Service-Robot/src/world/map.yaml" & 
 sleep 5
 xterm  -e  "source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 10
